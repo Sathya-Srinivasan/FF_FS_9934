11b181514157de504e5ac2117c9689588c8394be
Sathya-Srinivasan <72451345+Sathya-Srinivasan@users.noreply.github.com>
2020-10-15 08:05:12 +0530

    Add files via upload

--- FoodOrder.java
+++ FoodOrder.java
@@ -1,124 +1,126 @@
 package com.hcl.day31b;
+
 import java.util.Date;
 
 /**
+ * Day :#32
+ * 
+ * Topic : Core Java
+ * 
+ * This class is used to create constructors, getter and setter methods for the
+ * given requirements.
+ * 
  * @author Sathya
  *
  */
 public class FoodOrder {
-  private Date orderDate;
-  private String items;
-  private String status;
-  private double price;
-  private String place;
-  private String orderOwner;
+	private Date orderDate;
+	private String items;
+	private String status;
+	private double price;
+	private String place;
+	private String orderOwner;
 
-/**
- * @param orderDate
- * @param items
- * @param status
- * @param price
- * @param place
- */
-public FoodOrder(String orderOwner,Date orderDate, String items, String status, double price, String place) {
-	super();
-	this.orderOwner = orderOwner;
-	this.orderDate = orderDate;
-	this.items = items;
-	this.status = status;
-	this.price = price;
-	this.place = place;
-}
+	/**
+	 * @param orderDate
+	 * @param items
+	 * @param status
+	 * @param price
+	 * @param place
+	 * @param orderOwner
+	 */
+	public FoodOrder(String orderOwner, Date orderDate, String items, String status, double price, String place) {
+		super();
+		this.orderOwner = orderOwner;
+		this.orderDate = orderDate;
+		this.items = items;
+		this.status = status;
+		this.price = price;
+		this.place = place;
+	}
 
+	/**
+	 * @return the orderOwner
+	 */
+	public String getOrderOwner() {
+		return orderOwner;
+	}
 
+	/**
+	 * @param orderOwner the orderOwner to set
+	 */
+	public void setOrderOwner(String orderOwner) {
+		this.orderOwner = orderOwner;
+	}
 
-/**
- * @return the orderOwner
- */
-public String getOrderOwner() {
-	return orderOwner;
-}
+	/**
+	 * @return the orderDate
+	 */
+	public Date getOrderDate() {
+		return orderDate;
+	}
 
+	/**
+	 * @param orderDate the orderDate to set
+	 */
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
 
+	/**
+	 * @return the items
+	 */
+	public String getItems() {
+		return items;
+	}
 
-/**
- * @param orderOwner the orderOwner to set
- */
-public void setOrderOwner(String orderOwner) {
-	this.orderOwner = orderOwner;
-}
+	/**
+	 * @param items the items to set
+	 */
+	public void setItems(String items) {
+		this.items = items;
+	}
 
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
 
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-/**
- * @return the orderDate
- */
-public Date getOrderDate() {
-	return orderDate;
-}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
 
-/**
- * @param orderDate the orderDate to set
- */
-public void setOrderDate(Date orderDate) {
-	this.orderDate = orderDate;
-}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
-/**
- * @return the items
- */
-public String getItems() {
-	return items;
-}
+	/**
+	 * @return the place
+	 */
+	public String getPlace() {
+		return place;
+	}
 
-/**
- * @param items the items to set
- */
-public void setItems(String items) {
-	this.items = items;
-}
+	/**
+	 * @param place the place to set
+	 */
+	public void setPlace(String place) {
+		this.place = place;
+	}
 
-/**
- * @return the status
- */
-public String getStatus() {
-	return status;
-}
-
-/**
- * @param status the status to set
- */
-public void setStatus(String status) {
-	this.status = status;
-}
-
-/**
- * @return the price
- */
-public double getPrice() {
-	return price;
-}
-
-/**
- * @param price the price to set
- */
-public void setPrice(double price) {
-	this.price = price;
-}
-
-/**
- * @return the place
- */
-public String getPlace() {
-	return place;
-}
-
-/**
- * @param place the place to set
- */
-public void setPlace(String place) {
-	this.place = place;
-}
-  
- 
 }
--- FoodOrderMain.java
+++ FoodOrderMain.java
@@ -1,21 +1,25 @@
 package com.hcl.day31b;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 /**
- *  Day :#31
- *  
+ * Day :#31
+ * 
  * Topic : Core Java
  * 
- * This class is used to order the food and performs add, read and update method
+ * This class is used to order the food and performs methods like add, read and
+ * update method
  * 
  * @author Sathya
  *
  */
 public class FoodOrderMain {
 	ArrayList<FoodOrder> food = new ArrayList<FoodOrder>();
-
+	 static Logger log = Logger.getLogger(FoodOrderMain.class.getName());  
 	public static void main(String[] args) {
 		FoodOrderMain foodOrderMain = new FoodOrderMain();
 		Scanner input = new Scanner(System.in);
@@ -24,7 +28,7 @@ public class FoodOrderMain {
 		try {
 			System.out.println("Welcome to Food Ordering");
 			do {
-				System.out.println("Enter the command(add/read/update/quit)");
+				System.out.println("Enter the command(add/read/update/delete/quit)");
 				choice = input.next();
 				switch (choice) {
 				case "add":
@@ -36,28 +40,38 @@ public class FoodOrderMain {
 				case "update":
 					foodOrderMain.update();
 					break;
-
+				case "delete":
+					foodOrderMain.delete();
+					break;
 				default:
-					System.out.println("Sorry but " + choice);
+					System.out.println("Sorry " + choice);
 				}
 
-			} while (!(choice.equals("quit")));
+			} while (!(choice.equals("Not Available")));
 		}
 
 		catch (Exception e) {
-			System.out.println("Valid data must be entered" + e.getMessage());
+			System.out.println("Give valid items" + e.getMessage());
 		}
 	}
 
+	/*
+	 * This method is used to add food details like Order owner, Items, Status,
+	 * Price, Place
+	 * 
+	 * @param input
+	 */
+
 	public void add() {
 		Scanner input = new Scanner(System.in);
+
 		Date dt = new Date();
 		System.out.println("Order Owner");
 		String orderOwner = input.nextLine();
-		System.out.println("Items");
+		System.out.println("items");
 		String items = input.nextLine();
 
-		System.out.println("Status");
+		System.out.println("status");
 		String status = input.nextLine();
 
 		System.out.println("price");
@@ -70,17 +84,33 @@ public class FoodOrderMain {
 
 	}
 
+	/*
+	 * This method is used to read the list of food items which have been ordered.
+	 * 
+	 * @param input
+	 */
+
 	public void read() {
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("List of Food items Ordered:");
+
 		for (FoodOrder fo : food) {
-			System.out.println(
-					"Order Owner:" + fo.getOrderOwner() + "|Order Date:" + fo.getOrderDate() + "|Items:" + fo.getItems()
-							+ "|Status:" + fo.getStatus() + "|Price:" + fo.getPrice() + "|Place:" + fo.getPlace());
+			System.out.println("Order Owner:" + fo.getOrderOwner());
+			System.out.println("Order Date:" + fo.getOrderDate());
+			System.out.println("Items:" + fo.getItems());
+			System.out.println("Status:" + fo.getStatus());
+			System.out.println("Price:" + fo.getPrice());
+			System.out.println("Place:" + fo.getPlace());
+
 		}
 	}
 
+	/*
+	 * This method is used to update the food order details
+	 * 
+	 * @param input
+	 */
 	public void update() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the Order Owner Name to be Edited:");
@@ -99,15 +129,38 @@ public class FoodOrderMain {
 
 		System.out.println("place");
 		String place = input.next();
-		food.removeIf(e -> e.getOrderOwner().equals(owner));
+
 		food.add(new FoodOrder(owner, dt, items, status, price, place));
 
-		// int index = food.indexOf(owner);
-		// food.set(index, new FoodOrder(owner, dt, items, status, price, place));
 		System.out.println("Order for " + owner + " has been updated successfully.");
 
 		/*
-		 * } else { System.out.println("No items found"); }
+		 * food.removeIf(e -> e.getOrderOwner().equals(owner)); } else {
+		 * System.out.println("No items found"); }
 		 */
+	}
+
+	public void delete() {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the Order Owner Name to be deleted:");
+		String items = input.nextLine();
+		// if (food.contains(owner)) {
+
+		// Date dt = new Date();
+		// System.out.println("Items");
+		// String items = input.nextLine();
+		/*
+		 * System.out.println("Status"); String status = input.nextLine();
+		 * 
+		 * System.out.println("price"); double price = input.nextDouble();
+		 * 
+		 * System.out.println("place"); String place = input.next();
+		 */
+
+		food.removeIf(e -> e.getOrderOwner().equals(items));
+
+		System.out.println("Order for " + items + " has been deleted successfully.");
+
 	}
 }
